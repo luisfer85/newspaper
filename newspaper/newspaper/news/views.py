218a1ad00161766ff9c2b121674204dc3527d785
luisfer85 <luisfer85@gmail.com>
2017-02-11 01:01:54 +0100

    Clase 3: Primera version de la lista de noticias (listado)

--- newspaper/newspaper/news/views.py
+++ newspaper/newspaper/news/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render_to_response
+from newspaper.news.models import News
+from datetime import datetime
 
 
 def news_list(request):
-    return render_to_response("news/news_list.html")
+    news = News.objects.filter(
+        publish_date__lte=datetime.now()).order_by('publish_date')
+    return render_to_response("news/news_list.html",
+        {'news': news})
